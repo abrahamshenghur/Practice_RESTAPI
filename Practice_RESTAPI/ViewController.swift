b66df29fb0feaee3ee09f5a1f5839520847fadb8
abrahamshenghur <ashenghur@gmail.com>
2020-10-10 04:47:24 -0700

    Initial commit

---
+++ Practice_RESTAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice_RESTAPI
+//
+//  Created by Abraham on 10/10/20.
+//  Copyright © 2020 Abraham Shenghur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Practice_RESTAPI
+
+#### Practice fetching data in JSON format and display content in a table view.
