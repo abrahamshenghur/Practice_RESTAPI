cd4131d993a44c61d820e6ee27a9ec65c88833b5
abrahamshenghur <ashenghur@gmail.com>
2020-10-10 04:53:13 -0700

    Delete storyboard for initial project setup

--- Practice_RESTAPI/AppDelegate.swift
+++ Practice_RESTAPI/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let homeViewController = ViewController()
+        homeViewController.view.backgroundColor = UIColor.red
+        window?.rootViewController = homeViewController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
